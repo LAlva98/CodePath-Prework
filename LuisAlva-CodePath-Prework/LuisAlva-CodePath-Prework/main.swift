b8cb015b647c0aea0c7b86426b52df2743766ce6
Luis Alva <lalva@csumb.edu>
2018-10-01 16:30:07 -0700

    first push

---
+++ LuisAlva-CodePath-Prework/LuisAlva-CodePath-Prework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LuisAlva-CodePath-Prework
+//
+//  Created by Digital NEST on 10/1/18.
+//  Copyright © 2018 Digital NEST. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
